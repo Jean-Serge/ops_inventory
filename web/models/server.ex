a632ed30c1b8e53933fc0aadfae3e19c1238d3eb
Jean-Serge Monbailly <jean-serge@derniercri.io>
2017-06-04 23:02:45 +0200

    Insert or update droplet

--- web/models/server.ex
+++ web/models/server.ex
@@ -1,9 +1,17 @@
 defmodule OpsInventory.Server do
   use OpsInventory.Web, :model
 
+  alias OpsInventory.{
+    Server,
+    Repo
+  }
+
+  # defstruct %{}
+
   schema "servers" do
     field :name, :string
     field :ip_address, :string
+    field :id_digital_ocean, :integer, unique: true
 
     timestamps()
   end
@@ -13,7 +21,17 @@ defmodule OpsInventory.Server do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:name, :ip_address])
+    |> cast(params, [:name, :ip_address, :id_digital_ocean])
     |> validate_required([:name, :ip_address])
+    |> unique_constraint(:id_digital_ocean)
+  end
+
+  def insert_or_update_droplet(droplet) do
+    case Repo.get_by(Server, id_digital_ocean: droplet.id_digital_ocean) do
+      nil     ->   struct(Server, droplet)
+      server  -> server
+    end
+    |> Server.changeset
+    |> Repo.insert_or_update!
   end
 end
