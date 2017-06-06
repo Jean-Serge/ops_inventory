38170063868e1fe692a7f5058d9762850fb91dda
Jean-Serge Monbailly <jean-serge@derniercri.io>
2017-06-07 00:48:44 +0200

    Add method to get status for all droplets

--- web/controllers/digital_ocean_controller.ex
+++ web/controllers/digital_ocean_controller.ex
@@ -17,4 +17,33 @@ defmodule OpsInventory.DigitalOceanController do
 
         json conn, %{ok: true}
     end
+
+    def check_droplet_availability do
+        droplets =
+            list_droplets()
+            |> Enum.map(fn(droplet) ->
+                %{
+                    id: droplet.id,
+                    status: droplet.status
+                }
+            end)
+
+        (
+            from s in Server,
+            where: not is_nil(s.id_digital_ocean),
+            select: %{
+                id: s.id,
+                id_digital_ocean: s.id_digital_ocean
+            }
+        )
+        |> Repo.all
+        |> Enum.map(fn(server = %{ id_digital_ocean: id_digital_ocean }) ->
+            status = 
+                Enum.find(droplets,
+                    fn(%{ id: id }) -> id === id_digital_ocean end
+                ).status
+            
+            Map.put(server, :status, status)
+        end)
+    end
 end
