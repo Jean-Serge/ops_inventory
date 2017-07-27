af5f50e76fb5ee9784e0b355ff5b910573b90c42
Jean-Serge Monbailly <jean-serge@derniercri.io>
2017-07-27 19:40:52 +0200

    Lint scripts

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,7 +11,7 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import 'phoenix_html'
 
 // Import local files
 //
@@ -20,4 +20,4 @@ import "phoenix_html"
 
 // import socket from "./socket"
 
-import "./event_handlers"
+import './event_handlers'
